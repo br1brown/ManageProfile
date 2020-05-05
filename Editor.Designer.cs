f3ef142c116dbc24ecf05ef3434feb73e4559cb1
Br1 Brown <50661545+br1brown@users.noreply.github.com>
2020-05-05 19:08:03 +0200

    Init

---
+++ Editor.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Profili {
+	partial class Editor {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Editor));
+			this.textName = new System.Windows.Forms.TextBox();
+			this.listProg = new System.Windows.Forms.ListBox();
+			this.save = new System.Windows.Forms.Button();
+			this.btneditadd = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.txtAdd = new System.Windows.Forms.TextBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.opnefile = new System.Windows.Forms.Label();
+			this.lDel = new System.Windows.Forms.Label();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// textName
+			// 
+			this.textName.Dock = System.Windows.Forms.DockStyle.Top;
+			this.textName.Location = new System.Drawing.Point(0, 0);
+			this.textName.Name = "textName";
+			this.textName.Size = new System.Drawing.Size(465, 22);
+			this.textName.TabIndex = 0;
+			this.textName.TextChanged += new System.EventHandler(this.textName_TextChanged);
+			// 
+			// listProg
+			// 
+			this.listProg.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listProg.FormattingEnabled = true;
+			this.listProg.ItemHeight = 16;
+			this.listProg.Location = new System.Drawing.Point(3, 18);
+			this.listProg.Name = "listProg";
+			this.listProg.Size = new System.Drawing.Size(459, 215);
+			this.listProg.TabIndex = 1;
+			this.listProg.SelectedIndexChanged += new System.EventHandler(this.listProg_SelectedIndexChanged);
+			// 
+			// save
+			// 
+			this.save.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.save.Enabled = false;
+			this.save.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.save.Location = new System.Drawing.Point(0, 362);
+			this.save.Name = "save";
+			this.save.Size = new System.Drawing.Size(465, 38);
+			this.save.TabIndex = 3;
+			this.save.Text = "SAVE";
+			this.save.UseVisualStyleBackColor = true;
+			this.save.Click += new System.EventHandler(this.save_Click);
+			// 
+			// btneditadd
+			// 
+			this.btneditadd.Dock = System.Windows.Forms.DockStyle.Right;
+			this.btneditadd.Location = new System.Drawing.Point(403, 18);
+			this.btneditadd.Name = "btneditadd";
+			this.btneditadd.Size = new System.Drawing.Size(53, 52);
+			this.btneditadd.TabIndex = 4;
+			this.btneditadd.Text = "+";
+			this.btneditadd.UseVisualStyleBackColor = true;
+			this.btneditadd.Click += new System.EventHandler(this.btneditadd_Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox1.Controls.Add(this.listProg);
+			this.groupBox1.Location = new System.Drawing.Point(0, 28);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(465, 236);
+			this.groupBox1.TabIndex = 5;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Program/File to Start";
+			// 
+			// txtAdd
+			// 
+			this.txtAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtAdd.Location = new System.Drawing.Point(9, 21);
+			this.txtAdd.Name = "txtAdd";
+			this.txtAdd.Size = new System.Drawing.Size(388, 22);
+			this.txtAdd.TabIndex = 7;
+			this.txtAdd.TextChanged += new System.EventHandler(this.txtAdd_TextChanged);
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox2.Controls.Add(this.opnefile);
+			this.groupBox2.Controls.Add(this.txtAdd);
+			this.groupBox2.Controls.Add(this.btneditadd);
+			this.groupBox2.Location = new System.Drawing.Point(3, 283);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(459, 73);
+			this.groupBox2.TabIndex = 2;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Add/Edit";
+			// 
+			// opnefile
+			// 
+			this.opnefile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.opnefile.AutoSize = true;
+			this.opnefile.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.opnefile.Location = new System.Drawing.Point(298, 46);
+			this.opnefile.Name = "opnefile";
+			this.opnefile.Size = new System.Drawing.Size(99, 17);
+			this.opnefile.TabIndex = 8;
+			this.opnefile.Text = "CHOOSE FILE";
+			this.opnefile.Click += new System.EventHandler(this.opnefile_Click);
+			// 
+			// lDel
+			// 
+			this.lDel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.lDel.AutoSize = true;
+			this.lDel.ForeColor = System.Drawing.Color.Red;
+			this.lDel.Location = new System.Drawing.Point(445, 3);
+			this.lDel.Name = "lDel";
+			this.lDel.Size = new System.Drawing.Size(17, 17);
+			this.lDel.TabIndex = 7;
+			this.lDel.Text = "X";
+			this.lDel.Click += new System.EventHandler(this.lDel_Click);
+			// 
+			// Editor
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(465, 400);
+			this.Controls.Add(this.lDel);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.save);
+			this.Controls.Add(this.textName);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.KeyPreview = true;
+			this.MinimumSize = new System.Drawing.Size(285, 345);
+			this.Name = "Editor";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Editor";
+			this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form_DragDrop);
+			this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form_DragEnter);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Editor_KeyDown);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textName;
+		private System.Windows.Forms.ListBox listProg;
+		private System.Windows.Forms.Button save;
+		private System.Windows.Forms.Button btneditadd;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.TextBox txtAdd;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Label lDel;
+		private System.Windows.Forms.Label opnefile;
+	}
+}
---
+++ Editor.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Profili {
+	public partial class Editor : Form {
+		public Editor(string name = "") {
+			InitializeComponent();
+			textName.Text = name;
+			textName.ReadOnly = textName.Text != "";
+			lDel.Visible = textName.ReadOnly;
+			if (textName.Text != "") {
+				listProg.Items.AddRange(Form1.GetItems(textName.Text));
+			}
+		}
+
+		private void save_Click(object sender, EventArgs e) {
+			Form1.SetItem(textName.Text, listProg.Items.OfType<string>().ToArray());
+			Close();
+		}
+
+		private void listProg_SelectedIndexChanged(object sender, EventArgs e) {
+			var menuVisible = (listProg.SelectedIndex != -1);
+
+			if (menuVisible) {
+				if (txtAdd.Text == "") {
+					txtAdd.Text = listProg.SelectedItem.ToString();
+					listProg.Items.RemoveAt(listProg.SelectedIndex);
+				} else {
+					SystemSounds.Exclamation.Play();
+					txtAdd.Focus();
+				}
+				listProg.SelectedIndex = -1;
+			}
+
+			save.Enabled = textName.Text != string.Empty && listProg.Items.Count > 0;
+		}
+
+		private void btneditadd_Click(object sender, EventArgs e) {
+			if (txtAdd.Text != "") {
+				listProg.Items.Add(txtAdd.Text);
+				txtAdd.Text = "";
+			}
+		}
+
+		private void textName_TextChanged(object sender, EventArgs e) {
+			save.Enabled = textName.Text != string.Empty && listProg.Items.Count > 0;
+		}
+
+		private void lDel_Click(object sender, EventArgs e) {
+			if ((MessageBox.Show("Do you really want to delete the profile?", "DELETE THE PROFILE?",
+   MessageBoxButtons.YesNo, MessageBoxIcon.Question,
+   MessageBoxDefaultButton.Button1) == System.Windows.Forms.DialogResult.Yes)) {
+				Form1.Dels(textName.Text);
+				Close();
+			}
+		}
+
+
+		void Form_DragEnter(object sender, DragEventArgs e) {
+			if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+		}
+
+		void Form_DragDrop(object sender, DragEventArgs e) {
+			listProg.Items.AddRange((string[])e.Data.GetData(DataFormats.FileDrop));
+		}
+		private void Editor_KeyDown(object sender, KeyEventArgs e) {
+			if (e.KeyCode == Keys.Escape) {
+				if (txtAdd.Text != string.Empty)
+					if ((MessageBox.Show("Close out without saving?", "CLOSE OUT WITHOUT SAVING?",
+		   MessageBoxButtons.YesNo, MessageBoxIcon.Question,
+		   MessageBoxDefaultButton.Button1) != System.Windows.Forms.DialogResult.Yes))
+						return;
+				this.Close();
+			}
+		}
+
+		private void opnefile_Click(object sender, EventArgs e) {
+			if (txtAdd.Text == string.Empty)
+				using (OpenFileDialog openFileDialog = new OpenFileDialog()) {
+					openFileDialog.InitialDirectory = "c:\\";
+					openFileDialog.Filter = "All files (*.*)|*.*";
+					openFileDialog.RestoreDirectory = true;
+
+					if (openFileDialog.ShowDialog() == DialogResult.OK) {
+						txtAdd.Text = (openFileDialog.FileName);
+					}
+				}
+		}
+
+		private void txtAdd_TextChanged(object sender, EventArgs e) {
+			save.Enabled = textName.Text != string.Empty && listProg.Items.Count > 0;
+		}
+	}
+}
